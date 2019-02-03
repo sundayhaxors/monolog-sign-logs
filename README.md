53221b4fae6d7ab98203bc1712a1a1e09ff6d914
Frank Berger <fberger@b-factor.de>
2019-02-03 22:11:51 +0100

    class content added

--- README.md
+++ README.md
@@ -20,11 +20,16 @@ use SundayHaxors\Monolog\SignLogs\Processor\SignLogsProcessor;
 $log = new Logger('logger');
 $log->pushHandler(new StreamHandler('path/to/log', Logger::WARNING));
 
-$processor = new SignLogsProcessor('YOUR-SECRET-KEY');
+$processor = new SignLogsProcessor('YOUR-SECRET-KEY','Hashing Algorithm');
 $log->pushProcessor($processor);
 
 $log->log(Logger::DEBUG, 'This is a log message');
 ```
+
+For the available Hashing Algorithms in your PHP Installation, please check the output of the function `hash_algos();`
+
+In case you use several Processors in your Stack, take care that the SignLogsProcessor is running last.
+
 ## License
 
 Package is licensed under the LGPL License - see the LICENSE file for details
--- src/Processor/SignLogsProcessor.php
+++ src/Processor/SignLogsProcessor.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * Created by PhpStorm.
  * User: frank
@@ -8,27 +8,10 @@
 
 namespace Sundayhaxors\Monolog\SignLogs\Processor;
 
+use Monolog\Processor\ProcessorInterface;
 
-class SignLogsProcessor
+class SignLogsProcessor implements ProcessorInterface
 {
-    
-    /*
-     *
-     * $record = [
-  'message' => '',
-  'level' => 200,
-  'level_name' => 'INFO',
-  'channel' => 'app',
-  'datetime' => DateTime::__set_state([
-     'date' => '2018-06-11 18:39:26.353995',
-     'timezone_type' => 3,
-     'timezone' => 'UTC',
-  ]),
-  'context' => [],
-  'extra' => []
-    add
-]
-     */
     
     /**
      * @var string Preshared Key
@@ -36,17 +19,43 @@ class SignLogsProcessor
     protected $key;
     
     /**
-     * @param string $key
+     * @var
      */
-    public function __construct(string $key) {
+    protected $algorithm;
     
+    /**
+     * SignLogsProcessor constructor.
+     *
+     * @param string $key
+     * @throws \InvalidArgumentException
+     */
+    public function __construct(string $key, string $algorithm)
+    {
+        if (empty($key)) {
+            throw new \InvalidArgumentException('Add a key');
+        }
+        if (empty($algorithm)) {
+            throw new \InvalidArgumentException('Define an Algorithm');
+        }
+        $supported_algorithm = \hash_algos();
+        if (!in_array($algorithm, $supported_algorithm)) {
+            throw new \InvalidArgumentException('Algorithm not supported by your PHP Installation. See output of function hash_algos()');
+        }
+        $this->key = $key;
+        $this->algorithm = $algorithm;
     }
     
-    public function __invoke(array $record): array {
-    
+    /**
+     * @param array $record
+     * @return array
+     */
+    public function __invoke(array $record): array
+    {
+        $serialized_record = \serialize($record);
+        $checksum = hash($this->algorithm, $this->key.':'.$serialized_record, false);
+        $record['extra']['signature'] = $checksum;
+        unset($checksum);
+        unset($serialized_record);
+        return $record;
     }
-    
-    
-    
-    
 }
