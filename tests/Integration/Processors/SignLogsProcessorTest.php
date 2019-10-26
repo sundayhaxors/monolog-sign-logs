ccd27cf90787dc7257e878059431cd7d3274e448
Florian Engelhardt <florian.engelhardt@oxid-esales.com>
2019-10-26 14:01:09 +0200

    add the first integration test

---
+++ tests/Integration/Processors/SignLogsProcessorTest.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types = 1);
+
+namespace SundayHaxors\Monolog\SignLogs\Tests\Integration\Processors;
+
+use Monolog\Logger;
+use Monolog\Handler\TestHandler;
+use PHPUnit\Framework\TestCase;
+use SundayHaxors\Monolog\SignLogs\Processor\SignLogsProcessor;
+
+class SignLogsProcessorTest extends TestCase
+{
+    /**
+     * @covers SundayHaxors\Monolog\SignLogs\Tests\Processor\SignLogsProcessor
+     */
+    public function testFoo()
+    {
+        $log = new Logger('logger');
+        $handler = new TestHandler(
+            Logger::DEBUG
+        );
+        $log->pushHandler($handler);
+
+        $algo = 'md5';
+        $secret = 'SECRET';
+
+        $processor = new SignLogsProcessor(
+            $secret,
+            $algo
+        );
+        $log->pushProcessor($processor);
+
+        $log->log(
+            Logger::DEBUG,
+            'This is a log message'
+        );
+
+        foreach ($handler->getRecords() as $record) {
+            $signature = $record['extra']['signature'];
+            unset($record['extra']['signature']);
+            unset($record['formatted']);
+            $serialized_record = \serialize($record);
+            $checksum = hash($algo, $secret.':'.$serialized_record, false);
+            $this->assertEquals($signature, $checksum);
+        }
+    }
+}
